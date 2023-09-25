ade697381e5cfe7288d30dd7e839154ec682f103
hadilkhe <121692875+hadilkhe@users.noreply.github.com>
2023-09-25 15:45:49 +0100

    completed asseignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function superbowlWin(record) {
+    const winRecord = record.find(game => game.result === "W");
+  
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+
+      return undefined;
+    }
+  }
+  
+  // Example 
+  const record = [
+    { year: "2018", result: "L" },
+    { year: "2017", result: "W" },
+    { year: "2016", result: "N/A" },
+    
+  ];
+  
+  const winYear = superbowlWin(record);
+  console.log(winYear); 
+  
